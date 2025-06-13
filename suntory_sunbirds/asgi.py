01305de9ed7d223e327a1ea01138acc90d3d70b0
jw_albert <ru04jo30801@gmail.com>
2025-06-14 03:55:55 +0800

    Migrate Django project to new settings module
    Remove legacy project files from 'fcu_shop' including settings, URLs, and configuration modules.
    Update `manage.py` to reference 'suntory_sunbirds.settings'.
    Adjust template inheritance to utilize the new base template structure.

--- suntory_sunbirds/asgi.py
+++ suntory_sunbirds/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for fcu_shop project.
+ASGI config for suntory_sunbirds project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fcu_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suntory_sunbirds.settings')
 
 application = get_asgi_application()
--- suntory_sunbirds/wsgi.py
+++ suntory_sunbirds/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for fcu_shop project.
+WSGI config for suntory_sunbirds project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fcu_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suntory_sunbirds.settings')
 
 application = get_wsgi_application()
