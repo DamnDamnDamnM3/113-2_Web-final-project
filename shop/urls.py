ada9db081134b6f43ccb1efe1127c80d1fac660b
JW-Albert <ru04jo30801@gmail.com>
2025-06-13 08:28:16 +0800

    Refactor cart URL and form action for consistency
    Renames the URL path for adding products to the cart from `add_product_to_cart` to `add_to_cart` in both the URL configuration and the product list template. This change enhances clarity and maintains consistency across the application.

--- shop/urls.py
+++ shop/urls.py
@@ -16,11 +16,11 @@ urlpatterns = [
     ),
     path("cart/", views.cart, name="cart"),
     path("clear_cart/", views.clear_cart, name="clear_cart"),
-    # path(
-    #     "add_product_to_cart/<int:product_id>/",
-    #     views.add_product_to_cart,
-    #     name="add_product_to_cart",
-    # ),
+    path(
+        "add_to_cart/<int:product_id>/",
+        views.add_to_cart,
+        name="add_to_cart",
+    ),
     path("update_cart/<int:product_id>/", views.update_cart, name="update_cart"),
     path(
         "remove_product_from_cart/<int:product_id>/",
@@ -40,5 +40,5 @@ urlpatterns = [
         name="remove_card_from_cart",
     ),
     path("history/", views.history, name="history"),
-    path("team/", views.team, name='team'),
+    path("team/", views.team, name="team"),
 ]
