483f81de113e11709211a016c692f3b2fea8cab1
jw_albert <ru04jo30801@gmail.com>
2025-06-14 16:37:49 +0800

    Update settings to load environment variables and modify email configuration
    - Added dotenv support to load environment variables in settings.py.
    - Updated email configuration to retrieve values from environment variables instead of hardcoding.
    - Updated .gitignore to reflect changes in the Django environment file.
    - Added python-dotenv to requirements.txt for environment variable management.

--- suntory_sunbirds/settings.py
+++ suntory_sunbirds/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 載入環境變數
+# Load environment variables
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -137,6 +142,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "jw.albert.tw@gmail.com"  # 請替換成你的 Gmail 信箱
-EMAIL_HOST_PASSWORD = "aaiinplyfmivjkos"  # 請替換成你的應用程式密碼
-DEFAULT_FROM_EMAIL = "jw.albert.tw@gmail.com"  # 請替換成你的 Gmail 信箱
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # 從環境變數讀取
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # 從環境變數讀取
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  # 從環境變數讀取
